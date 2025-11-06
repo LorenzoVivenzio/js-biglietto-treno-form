c7d1c62d8ffb5fe139a3ee699393054141a160a5
LorenzoVivenzio <lorenzovivenzio988@hotmail.com>
2025-11-06 16:21:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// const minore = 18;
+// const over = 60;
+// let kilometriPercorsiInput = prompt("Quanti kilometri vuoi percorrere?"); // string
+// const kilometriPercorsi = parseFloat(kilometriPercorsiInput); // decimal
+// // console.log(parseInt(kilometriPercorsi));
+
+// let etaUtenteInput = prompt("quanti anni hai?"); // string
+// const etaUtente = parseInt(etaUtenteInput); // decimal
+// // console.log(parseInt(etàUtente));
+
+// let prezzoPerKm = 0.21; // decimal
+// // console.log(prezzo);
+
+// let prezzoBiglietto = kilometriPercorsi * prezzoPerKm; // decimal
+// let scontoPrezzoMin = prezzoBiglietto * 0.20;
+// let bigliettoScontoMin = prezzoBiglietto - scontoPrezzoMin;
+// let ScontoPrezzoOver = prezzoBiglietto * 0.40;
+// let bigliettoScontoOver = prezzoBiglietto - ScontoPrezzoOver;
+
+// let risultato = "";
+// if (etaUtente < minore){
+
+//     console.log("Il prezzo del tuo biglietto è" + " " + bigliettoScontoMin + "$")
+
+// }else if(etaUtente < over){
+
+//     console.log(prezzoBiglietto + "$")
+
+// }else{
+
+//    console.log("Il prezzo del tuo biglietto è " + " " +  bigliettoScontoOver + "$")
+// }
+

9a2ae8d634f6844ad3b72ff532ad7297ecee1e31
LorenzoVivenzio <lorenzovivenzio988@hotmail.com>
2025-11-06 18:52:39 +0100

    add prezzo biglietto finale

--- script.js
+++ script.js
@@ -8,14 +8,8 @@
 // const etaUtente = parseInt(etaUtenteInput); // decimal
 // // console.log(parseInt(etàUtente));
 
-// let prezzoPerKm = 0.21; // decimal
-// // console.log(prezzo);
 
-// let prezzoBiglietto = kilometriPercorsi * prezzoPerKm; // decimal
-// let scontoPrezzoMin = prezzoBiglietto * 0.20;
-// let bigliettoScontoMin = prezzoBiglietto - scontoPrezzoMin;
-// let ScontoPrezzoOver = prezzoBiglietto * 0.40;
-// let bigliettoScontoOver = prezzoBiglietto - ScontoPrezzoOver;
+// console.log(prezzo);
 
 // let risultato = "";
 // if (etaUtente < minore){
@@ -30,4 +24,54 @@
 
 //    console.log("Il prezzo del tuo biglietto è " + " " +  bigliettoScontoOver + "$")
 // }
+
+const form = document.querySelector("form")
+const usernameInput = document.getElementById("utente")
+const kmPercorrereInput = document.getElementById("kmPercorrere")
+const etaInput = document.getElementById("eta")
+const inviaInput = document.getElementById("submit")
+const annullaInput = document.getElementById("annulla")
+const passeggero = document.querySelector(".passeggero")
+const scontoBiglietto = document.querySelector(".scontoBiglietto")
+const prezzoBiglietto = document.querySelector(".prezzoBiglietto")
+
+let scontoMin = 0.20;
+let scontoOver = 0.40;
+let prezzoKm = 0.21;
+
+
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+
+
+    const username = usernameInput.value;
+    const kmPercorrere = kmPercorrereInput.value;
+    const eta = etaInput.value;
+
+    let prezzoKmPercorsoScontoMin = (kmPercorrere * prezzoKm) * scontoMin;
+    let bigliettoScontoMinori = (kmPercorrere * prezzoKm) - prezzoKmPercorsoScontoMin; //biglietto con sconto Under
+    let prezzoKmPercorsoScontoOver = (kmPercorrere * prezzoKm) * scontoOver;
+    let bigliettoScontoOver = (kmPercorrere * prezzoKm) - prezzoKmPercorsoScontoOver; //biglietto con sconto Over65
+
+
+
+    if (eta === "under") {
+        passeggero.innerText = username;
+        scontoBiglietto.innerHTML = "20%"
+        prezzoBiglietto.innerHTML = bigliettoScontoMinori.toFixed(1) + "$"
+    } else if (eta === "over") {
+        passeggero.innerText = username;
+        scontoBiglietto.innerHTML = "40%"
+        prezzoBiglietto.innerHTML = bigliettoScontoOver.toFixed(1) + "$"
+    } else if (eta === "nessunaScelta") {
+        passeggero.innerText = username;
+        scontoBiglietto.innerHTML = "Nessun sconto"
+        prezzoBiglietto.innerHTML = (kmPercorrere * prezzoKm) + "$"
+    }
+
+})
+
 

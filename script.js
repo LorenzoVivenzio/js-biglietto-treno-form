db15600c30d041d4feb81125ca532a40f7769a78
LorenzoVivenzio <lorenzovivenzio988@hotmail.com>
2025-11-07 09:06:39 +0100

    add biglietto

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// const minore = 18;
-// const over = 60;
-// let kilometriPercorsiInput = prompt("Quanti kilometri vuoi percorrere?"); // string
-// const kilometriPercorsi = parseFloat(kilometriPercorsiInput); // decimal
-// // console.log(parseInt(kilometriPercorsi));
-
-// let etaUtenteInput = prompt("quanti anni hai?"); // string
-// const etaUtente = parseInt(etaUtenteInput); // decimal
-// // console.log(parseInt(etàUtente));
-
-
-// console.log(prezzo);
-
-// let risultato = "";
-// if (etaUtente < minore){
-
-//     console.log("Il prezzo del tuo biglietto è" + " " + bigliettoScontoMin + "$")
-
-// }else if(etaUtente < over){
-
-//     console.log(prezzoBiglietto + "$")
-
-// }else{
-
-//    console.log("Il prezzo del tuo biglietto è " + " " +  bigliettoScontoOver + "$")
-// }
-
 const form = document.querySelector("form")
 const usernameInput = document.getElementById("utente")
 const kmPercorrereInput = document.getElementById("kmPercorrere")
@@ -34,6 +7,8 @@ const annullaInput = document.getElementById("annulla")
 const passeggero = document.querySelector(".passeggero")
 const scontoBiglietto = document.querySelector(".scontoBiglietto")
 const prezzoBiglietto = document.querySelector(".prezzoBiglietto")
+let passeggeroCarrozzaElem = document.querySelector(".carrozza")
+
 
 let scontoMin = 0.20;
 let scontoOver = 0.40;
@@ -45,33 +20,34 @@ let prezzoKm = 0.21;
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-
-
     const username = usernameInput.value;
     const kmPercorrere = kmPercorrereInput.value;
     const eta = etaInput.value;
+    
+    console.log(eta, kmPercorrere, username)
 
     let prezzoKmPercorsoScontoMin = (kmPercorrere * prezzoKm) * scontoMin;
     let bigliettoScontoMinori = (kmPercorrere * prezzoKm) - prezzoKmPercorsoScontoMin; //biglietto con sconto Under
     let prezzoKmPercorsoScontoOver = (kmPercorrere * prezzoKm) * scontoOver;
     let bigliettoScontoOver = (kmPercorrere * prezzoKm) - prezzoKmPercorsoScontoOver; //biglietto con sconto Over65
 
-
-
     if (eta === "under") {
         passeggero.innerText = username;
         scontoBiglietto.innerHTML = "20%"
         prezzoBiglietto.innerHTML = bigliettoScontoMinori.toFixed(1) + "$"
+        passeggeroTicket.innerHTML = username;
+        passeggeroCarrozzaElem.innerHTML = Math.floor(Math.random()* 10) +1;
     } else if (eta === "over") {
         passeggero.innerText = username;
         scontoBiglietto.innerHTML = "40%"
         prezzoBiglietto.innerHTML = bigliettoScontoOver.toFixed(1) + "$"
+        carrozza.innerHTML = Math.floor(Math.random() * 10) +1;
     } else if (eta === "nessunaScelta") {
         passeggero.innerText = username;
         scontoBiglietto.innerHTML = "Nessun sconto"
         prezzoBiglietto.innerHTML = (kmPercorrere * prezzoKm) + "$"
+        carrozza.innerHTML = Math.floor(Math.random() * 10) +1;
     }
-
 })
 
 
